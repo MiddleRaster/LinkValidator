21c407206f2d8b7fe21b6f10aa10d8f11008de4e
Bill <Bill@MINI25>
2026-01-26 00:04:04 -0500

    Add project files.

---
+++ LinkValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkValidator
+{
+    public class LinkValidator
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
