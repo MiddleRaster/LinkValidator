882beafe1202f714323c6050a2eb911a01c207f3
MiddleRaster <MiddleRaster@users.noreply.github.com>
2026-01-26 13:26:21 -0500

    getting baseURL properly

--- LinkValidator.cs
+++ LinkValidator.cs
@@ -15,12 +15,11 @@ namespace LinkValidator
 
             var pending = new Queue<string>();
             var visited = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            pending.Enqueue(args[0]);
+            visited.Add    (args[0]);
 
-            string domain = UrlParser.GetDomain(args[0]);
-
-            string baseURL = args[0]; // not necessarily the root
-            pending.Enqueue(baseURL);
-            visited.Add    (baseURL);
+            string domain  = UrlParser.GetDomain   (args[0]);
+            string baseURL = UrlParser.GetDirectory(args[0]); // not necessarily the root
 
             while (pending.Count > 0)
             {
@@ -33,7 +32,7 @@ namespace LinkValidator
                 if (html != null)
                 {
                     // only recurse if in our domain
-                    if (!url.StartsWith(baseURL))
+                    if (!url.StartsWith(baseURL, StringComparison.OrdinalIgnoreCase))
                         continue;
 
                     List<string> links = HrefExtractor.ExtractAllHrefs(html); // now extract all the URLs from the html string
