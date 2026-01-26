18b7d4c5763bfaaea60e46e471fdb72bf5f67e4a
MiddleRaster <MiddleRaster@users.noreply.github.com>
2026-01-26 13:40:01 -0500

    added UerAgent string

--- HtmlFetcher.cs
+++ HtmlFetcher.cs
@@ -2,6 +2,14 @@
 {
     private static readonly HttpClient client = new HttpClient();
 
+    static HtmlFetcher()
+    {
+        client.DefaultRequestHeaders.UserAgent.ParseAdd(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+            "AppleWebKit/537.36 (KHTML, like Gecko) " +
+            "Chrome/120.0.0.0 Safari/537.36");
+    }
+
     public static async Task<string?> FetchHtmlAsync(string url)
     {
         if (!Uri.IsWellFormedUriString(url, UriKind.Absolute)) // TODO: this isn't right, because I will have incremental URLs, too.
